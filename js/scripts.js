d87598e40a8ba94a8aa1ebcc4a2661f5f64b8308
vitorbarbieri <vbarbierisilva@gmail.com>
2023-08-27 18:15:11 -0300

    07-tipos-dados-null-undefined

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
-var teste1 = 20 > 2; // true
-var teste2 = 2 >= 2; // true
-var teste3 = 5 < 5; // false
-var teste4 = 5 <= 10; // true
-var teste5 = 20 == 20; // true
-var teste6 = 'ivan' == 'joão' // false
-var teste7 = 20 === "20"; // false - valor é igual mas o tipo não. O primeiro é número, o segundo é string.
-var teste8 = 20 != 10; // true
-var teste9 = 10 != 10; // false
-var teste10 = 10 !== 10; // false - o tipo e valor são iguais
-var teste10 = "10" !== 10; // true - o tipo não é igual
-var teste11 = "9" !== 10; // false - o tipo nem o valor são iguais.
+//  undefined
 
-// Como consultar o tipo de uma variável?
+var undf;
+console.log(undf);
+console.log("Type: " + typeof undf);
 
-var v_string = "teste";
-var v_numero = 4.55;
-var idade = 21;
-var v_boolean = idade > 18 ;
+var nome = "Pedro";
+console.log(nome[0]); // O console mostrará "P"
+console.log(nome[4]); // O console mostrará "o"
+console.log(nome[10]); // O console mostrará undefined
 
-console.log(typeof v_string);  // retorna string
-console.log(typeof v_numero);  // retorna number
-console.log(typeof v_boolean);  // retorna boolean
+//  null
+
+var temperatura = 35;
+console.log(temperatura);
+
+temperatura = null;
+console.log(temperatura); // O console mostrará null
+
+//  null x undefined
+
+console.log(temperatura == undefined);
+console.log(temperatura === undefined);
+console.log("Type: " + typeof temperatura);
