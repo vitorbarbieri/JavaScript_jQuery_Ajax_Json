9727ba987386291115ad21959804c8ba1976044c
vitorbarbieri <vbarbierisilva@gmail.com>
2023-08-27 19:08:51 -0300

    10-trabalhando-com-o-DOM

---
+++ js/scripts2.js
@@ -0,0 +1,6 @@
+//  Aula 10 - Trabalhando com o DOM
+
+var conteudo_caixa = document.getElementById("caixa_azul").innerHTML;
+document.getElementById("caixa_azul").innerHTML = "<h1>" + conteudo_caixa + "</h1>"; 
+
+document.getElementById("caixa_amarela").innerHTML = "<h1>Caixa amarela</h1>"; 
