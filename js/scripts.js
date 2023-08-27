9debcb678061d8af163e95c8b8723116b3e645c8
vitorbarbieri <vbarbierisilva@gmail.com>
2023-08-27 17:39:41 -0300

    04-tipos-dados-strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
-var latitude = 40.87663;
-var longitude = -8.08373;
+var nome = "João";  // podem ser usadas aspas duplas
+var sobrenome = 'Gomes';  // ou aspas simples
+var cpf = '111.111.111-11';  
+var telefone = "998887655";  // por mais que só tenha números, como não é usado para realizar cálculos, deve ser do tipo string
+var ddd = '21';
+var email = 'joao@gmail.com';
+var biografia = "João começou a sua carreira como desenvolvedor web em 2010 e já participou de importantes projetos, como...";
 
-console.log("Latitude: " + latitude);
-console.log("Longitude: " + longitude);
+var nome_completo = nome + " " + sobrenome;
+console.log("Nome completo: " + nome_completo);
 
+console.log("(" + ddd + ")" + telefone);
+
+console.log("Primeira letra do nome: " + nome[0]);
+console.log("Última letra do nome: " + nome[nome.length - 1]);
+console.log("Tamanho do telefone: " + telefone.length);
