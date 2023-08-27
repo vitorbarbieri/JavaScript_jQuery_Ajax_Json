64fa1193b1f742ec24d2bb6ecd9306741de422a7
vitorbarbieri <vbarbierisilva@gmail.com>
2023-08-27 19:26:57 -0300

    11-funcoes

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,6 +1,18 @@
-//  Aula 10 - Trabalhando com o DOM
-
+//  ------------------------------ Aula 10 - Trabalhando com o DOM
+/* 
 var conteudo_caixa = document.getElementById("caixa_azul").innerHTML;
 document.getElementById("caixa_azul").innerHTML = "<h1>" + conteudo_caixa + "</h1>"; 
 
 document.getElementById("caixa_amarela").innerHTML = "<h1>Caixa amarela</h1>"; 
+ */
+//  ------------------------------ Aula 11 - Funções
+
+function valorIMC (peso, altura) {
+    return peso / (Math.pow(altura, 2));
+}
+
+var meuPeso = parseFloat(document.getElementById("peso").innerHTML); // Sempre bom converter o conteúdo do HTML para número, caso for realizar cálculos
+var minhaAltura = parseFloat(document.getElementById("altura").innerHTML);
+
+var imc = valorIMC(meuPeso, minhaAltura).toFixed(2);
+document.getElementById("imc").innerHTML = imc;
