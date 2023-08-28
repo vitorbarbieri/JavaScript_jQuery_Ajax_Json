cac72fbe34c7a462e0b5aed8f6b26f2d6c4188e6
vitorbarbieri <vbarbierisilva@gmail.com>
2023-08-28 16:44:02 -0300

    18-outros-get-element

--- js/scripts3.js
+++ js/scripts3.js
@@ -19,8 +19,22 @@ function button_clicked() {
 }
  */
 //  ------------------------------ Aula 17 - Manupulação do CSS
-
+/* 
 document.getElementById("botao_cor").onclick = function() {
     this.style['background-color'] = "purple"; // contem traço, não funciona o ".", não obedece a nomeclatura
     this.style.transform = "translateX(100px)";
 };
+*/
+//  ------------------------------ Aula 18 - Outros Métodos getElement
+
+var exemplo = document.getElementsByClassName("exemplo");
+exemplo[0].innerHTML = "Teste 1"
+exemplo[1].style.color = "red";
+exemplo[2].style['background-color'] = "purple";
+exemplo[2].style.color = "#fff";
+
+var exemplo = document.getElementsByTagName("p");
+exemplo[4].innerHTML = "Teste 1"
+exemplo[5].style.color = "red";
+exemplo[6].style['background-color'] = "purple";
+exemplo[6].style.color = "#fff";
