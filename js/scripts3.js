e1aeb24442a2e6ec7d4d32f30de4ea61ee25c2f0
vitorbarbieri <vbarbierisilva@gmail.com>
2023-08-28 17:33:29 -0300

    22-aninhamento-loops-condicionais

--- js/scripts3.js
+++ js/scripts3.js
@@ -81,7 +81,7 @@ do {
 } while (count2 < 5)
 */
 //  ------------------------------ Aula 21 - Condicionais (if, else if, else)
-
+/* 
 var idade = 17;
 if (idade < 18) {
     console.log('Menor de idade');
@@ -118,4 +118,46 @@ if (nome) {
     console.log(nome);
 } else {
     console.log("Nome não informado");
+}
+*/
+//  ------------------------------ Aula 25 - Aninhamento de loops e condicionais (nesting)
+
+var socio = false;
+var idade = 25;
+if (socio == true || idade >= 65) {
+    console.log('O ingresso é grátis');
+} else {
+    if (idade < 18) {
+        console.log('O ingresso custa: R$ 6,00');
+    } else {
+        console.log('O ingresso custa: R$ 12,00');
+    }
+}
+
+var funcionarios = [
+    {
+        'nome': 'Carlos Henrique da Silva',
+        'idade': 45,
+        'filhos': ['Mariana Alves da Silva', 'Fernanda Alves da Silva']
+    },
+    {
+        'nome': 'Maria Helena Pereira',
+        'idade': 32,
+        'filhos': undefined
+    },
+    {
+        'nome': 'José Feliciano Maia',
+        'idade': 39,
+        'filhos': ['Felipe Ferreira Maia', 'Fábio Ferreira Maia', 'João Ferreira Maia']
+    }
+];
+var list_element = document.getElementById("filhos");
+list_element.innerHTML = "";
+for (var a = 0 ; a < funcionarios.length ; a++) {
+    if (funcionarios[a].filhos) {
+        var lista_filhos = funcionarios[a].filhos;
+        for (var b = 0 ; b < lista_filhos.length ; b++) {
+            list_element.innerHTML += '<li>' + lista_filhos[b] + ' - Filho(a) de ' + funcionarios[a].nome + '</li>';
+        }
+    }
 }
