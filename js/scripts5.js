2987fbe28f1706bf3e8c5c14837dc4c34f4da996
vitorbarbieri <vbarbierisilva@gmail.com>
2023-09-04 19:38:46 -0300

    35-jquery-manipulacao-html

--- js/scripts5.js
+++ js/scripts5.js
@@ -1,3 +1,5 @@
+//  ---------------------------------------- Aula 52
+/* 
 // $("div").ação(); -> Seleciona todos os elementos DIV
 // $("#exemplo").ação(); -> Seleciona o elemento com id="exemplo"
 // $(".exemplo").ação(); -> Seleciona todos os elementos com a class="exemplo"
@@ -10,3 +12,36 @@ $("#esconder").click(function () {
         $(".exemplo").hide();
     }
 });
+*/
+//  ---------------------------------------- Aula 53
+
+var conteudo_html = $("#paragrafo-html").html();
+console.log(conteudo_html);
+var conteudo_texto = $("#paragrafo-html").text();
+console.log(conteudo_texto);
+
+$("#paragrafo-html").html("Mudar conteúdo do parágrafo");
+conteudo_html = $("#paragrafo-html").html();
+console.log(conteudo_html);
+$("#paragrafo-html").text("Mudar conteúdo do parágrafo");
+conteudo_texto = $("#paragrafo-html").html();
+console.log(conteudo_texto);
+
+
+var url_link = $("#paragrafo-link").attr("href");
+console.log(url_link);
+
+$("#paragrafo-link").attr("href", "https://www.google.com.br")
+url_link = $("#paragrafo-link").attr("href");
+console.log(url_link);
+
+
+$("#mudar_imagem").click(function () {
+    $("#imagem_js").attr("src", "../img/flamengo.png");
+});
+
+
+// Remover o conteúdo, mantendo o elemento:
+$("#paragrafo-empty").empty();
+// Remover o elemento:
+$("#paragrafo-empty").remove();
